5be1a557c7818689c91815eaecd77b7c7b2dc8ba
John Tucker <jtucker@spotify.com>
2022-03-26 15:21:22 -0400

    passing stuff

--- fpi-listing.php
+++ fpi-listing.php
@@ -26,7 +26,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>FPI - Listing</title>
-    <script defer="defer" src="<?php echo plugin_dir_url(__FILE__); ?>fpi-listing/static/js/main.fd036bc7.js"></script>
+    <script defer="defer" src="<?php echo plugin_dir_url(__FILE__); ?>fpi-listing/static/js/main.1a7c1641.js"></script>
   </head>
   <body>
     <div id="root"></div>
