7c20562705884ad611ac1ea2bf472b90d3759e62
John Tucker <jtucker@spotify.com>
2022-03-26 14:48:20 -0400

    cp

--- fpi-listing.php
+++ fpi-listing.php
@@ -1,3 +1,25 @@
+<?php
+  $indicators = array();
+  $args = array(
+    'post_type' => 'fpi_indicator',
+    'posts_per_page' => -1,
+  );
+  $loop = new WP_Query( $args );
+  while ( $loop->have_posts() ) : $loop->the_post();
+    $indicators[] = array(
+      'id' => get_the_ID(),
+      'link' => get_permalink(),
+      'name' => get_field('name'),
+      'image' => get_field('image'),
+      'latitude' => get_field('latitude'),
+      'longitude' => get_field('longitude'),
+      'ecological' => get_field('ecological'),
+      'economic' => get_field('economic'),
+      'community' => get_field('community')
+    );
+  endwhile;
+  wp_reset_query();
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -8,5 +30,8 @@
   </head>
   <body>
     <div id="root"></div>
+    <script>
+      window.fpiIndicators = <?php echo json_encode( $indicators ); ?>;
+    </script>
   </body>
 </html>
